0a70fbd1da9b73b73103c34c3769ea1430a93036
koro2koro <koro2koro@gmail.com>
2018-08-07 00:03:25 +0900

    add Controller and View of User

---
+++ conf/db/migration/default/common/V2__Add_Users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test1', 'test1@test.com', '$2a$10$xdFi81rcM/JqO0JieFTm8.cRArCKrDrdN7xXFwNRwrzn2iNYTPCr6', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test2', 'test2@test.com', '$2a$10$xdFi81rcM/JqO0JieFTm8.sN9SgdgRySh05I.5u.cN5Ar8r9CasU6', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test3', 'test3@test.com', '$2a$10$xdFi81rcM/JqO0JieFTm8.Nv5TOciF0wiURV3Ov3u09lEkzB5gnu.', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test4', 'test4@test.com', '$2a$10$xdFi81rcM/JqO0JieFTm8.KRb/AeaWLCXwVc6DmQUBwtixAJ0kkRu', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test5', 'test5@test.com', '$2a$10$xdFi81rcM/JqO0JieFTm8.x80gciE3dzRCnCsDrUIY2Cz5xFd3lL2', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test6', 'test6@test.com', '$2a$10$xdFi81rcM/JqO0JieFTm8.zhMyi4AXRGvGvOLtfZwu7fwZLDakba6', now(), now());
