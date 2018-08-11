272c3e186e526f1666b47807ca5c6effa227873f
koro2koro <koro2koro@gmail.com>
2018-08-11 11:20:03 +0900

    post

--- app/services/UserServiceImpl.scala
+++ app/services/UserServiceImpl.scala
@@ -1,9 +1,9 @@
 package services
 
 import javax.inject.Singleton
-
-import models.User
-import scalikejdbc.{ AutoSession, DBSession }
+import models.{PagedItems, User}
+import scalikejdbc.{AutoSession, DBSession}
+import skinny.Pagination
 
 import scala.util.Try
 
@@ -20,11 +20,15 @@ class UserServiceImpl extends UserService {
       User.where('email -> email).apply().headOption
     }
 
-  override def findAll(implicit dbSession: DBSession): Try[List[User]] = Try {
-    User.findAll()
+  def findAll(pagination: Pagination)(implicit dbSession: DBSession = AutoSession): Try[PagedItems[User]] = Try {
+    PagedItems[User](
+      pagination,
+      User.countAllModels(),
+      User.findAllWithPagination(pagination, Seq(User.defaultAlias.id.asc))
+    )
   }
 
-  override def findById(id: Long)(implicit dbSession: DBSession): Try[Option[User]] = Try {
+  def findById(id: Long)(implicit dbSession: DBSession): Try[Option[User]] = Try {
     User.findById(id)
   }
 
